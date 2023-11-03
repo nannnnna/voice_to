4bed58e8e0bb5b8b0713aa9a8ac634ccaf089638
nannnnna <141238876+nannnnna@users.noreply.github.com>
2023-11-03 14:40:31 +0300

    done

--- vosk-regnize.py
+++ vosk-regnize.py
@@ -12,14 +12,38 @@ async def read_dir():
     if len(sys.argv) == 1: 
         print('Invalid arg')
         return
-    if  not os.path.exists(sys.argv[1]):
+    if not os.path.exists(sys.argv[1]):
         print('Err path!')
         return
     files = os.listdir(sys.argv[1])
+    
+    # Если JSON файлы не существуют, записать их
+    if not has_json_files(sys.argv[1]):
+        tasks = []
+        for file in files:
+            if '.wav' in file:
+                tasks.append(recognize(sys.argv[1]+'/'+file))
 
-    for file in files:
-        if '.wav' in file:
-           await recognize(sys.argv[1]+'/'+file)
+        # Ограничение на количество одновременно запущенных задач (например, 5)
+        sem = asyncio.Semaphore(5)
+
+        async def process_with_semaphore(task):
+            async with sem:
+                return await task
+
+        await asyncio.gather(*(process_with_semaphore(task) for task in tasks))
+
+    # Независимо от того, были ли созданы JSON файлы или нет, извлеките текст и удалите файлы JSON
+    extract_all_phrase_to_text(sys.argv[1])
+
+    
+def has_json_files(directory):
+    """Проверяет директорию на наличие файлов с расширением .json."""
+    for filename in os.listdir(directory):
+        if filename.endswith('.json'):
+            return True
+    return False
+
       
 async def recognize(file):
     async with websockets.connect(uri_ws) as websocket:
@@ -42,6 +66,7 @@ async def recognize(file):
 
         await websocket.send('{"eof" : 1}')
         vosk_result.append(json.loads(await websocket.recv())) 
+        
         await write_data(vosk_result, file)
         print('---end ' + file +'---')
 
@@ -67,6 +92,31 @@ async def write_data(data, file_name):
     file_name = file_name[:file_name.find('.')] + '.json'
     with open(file_name, "w", encoding="utf-8") as file:
         json.dump(write_data, file, ensure_ascii=False, indent=4)
+        
+def extract_all_phrase_to_text(directory):
+    all_phrases = []
+    
+    # Извлечение 'all_phrase' из каждого файла
+    for filename in os.listdir(directory):
+        if filename.endswith('.json'):
+            with open(os.path.join(directory, filename), 'r', encoding='utf-8') as file:
+                data = json.load(file)
+                for entry in data:
+                    if 'all_phrase' in entry:
+                        all_phrases.append(entry['all_phrase'])
+
+    # Добавление извлеченных фраз в существующий текстовый файл (или создание нового, если он не существует)
+    with open(os.path.join(directory, 'all_phrases.txt'), 'a', encoding='utf-8') as file:  # Используйте 'a' для добавления
+        for phrase in all_phrases:
+            if phrase.strip():  # Исключение пустых строк
+                file.write(f"1 {phrase}\n")
+
+    # Удаление исходных файлов JSON
+    for filename in os.listdir(directory):
+        if filename.endswith('.json'):
+            os.remove(os.path.join(directory, filename))
+
+            
     
 asyncio.run(read_dir())
 
